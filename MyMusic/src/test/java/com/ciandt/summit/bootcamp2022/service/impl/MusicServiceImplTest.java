f115d16288921457ed1f6ec8a32b6d394c40f85f
Lucas A. Cruz <cruzlucas@ciandt.com>
2022-09-01 16:12:22 -0300

    BOOT-926-SHARE  - Foi feito a injeçao correta do Mapstruct no teste casa  - Adicionado esqueleto simples para teste de Exception

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/impl/MusicServiceImplTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/impl/MusicServiceImplTest.java
@@ -4,29 +4,37 @@ import com.ciandt.summit.bootcamp2022.dto.ArtistDTO;
 import com.ciandt.summit.bootcamp2022.dto.MusicDTO;
 import com.ciandt.summit.bootcamp2022.entity.Artist;
 import com.ciandt.summit.bootcamp2022.entity.Music;
+import com.ciandt.summit.bootcamp2022.exception.MusicNotFound;
 import com.ciandt.summit.bootcamp2022.repository.MusicRepository;
 import com.ciandt.summit.bootcamp2022.service.mapper.ArtistDTOMapper;
 import com.ciandt.summit.bootcamp2022.service.mapper.MusicDTOMapper;
-import com.ciandt.summit.bootcamp2022.service.mapper.MusicDTOMapperImpl;
-import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 class MusicServiceImplTest {
 
     @InjectMocks
@@ -35,42 +43,47 @@ class MusicServiceImplTest {
     @Mock
     private MusicRepository musicRepository;
 
-    private MusicDTOMapper musicDTOMapper = MusicDTOMapperImpl.INSTANCE;
+    @InjectMocks
+    private MusicDTOMapper INSTANCE = MusicDTOMapper.INSTANCE;
 
-    //@Spy
-    //private MusicDTOMapper musicDTOMapper = Mappers.getMapper(MusicDTOMapper.class);
-
+    private Sort sort;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         musicService = new MusicServiceImpl(musicRepository);
+
     }
 
-    public void init() {
-        musicDTOMapper = Mappers.getMapper(MusicDTOMapper.class);
+    @BeforeEach
+    public void mapStructSetup() {
+        INSTANCE = Mappers.getMapper(MusicDTOMapper.class);
         ArtistDTOMapper artistDTOMapper = Mappers.getMapper(ArtistDTOMapper.class);
-        ReflectionTestUtils.setField(musicDTOMapper, "musicDTOMapper", artistDTOMapper);
+        ReflectionTestUtils.setField(INSTANCE, "artistDTOMapper", artistDTOMapper);
     }
 
-    public Set<Music> buildMusicSet() {
 
+    public Set<Music> buildEntitySet(String artistName, String musicName) {
         Set<Music> set = new HashSet();
         Artist artist = new Artist();
-        artist.setId(new UUID(3, 10));
-        artist.setName("Engenheiros");
-        Music music = new Music();
-        music.setId(new UUID(3, 10));
-        music.setName("Era Um Garoto Que Como Eu Amava Os Beatles");
-        music.setArtist(artist);
+        artist.setId(UUID.nameUUIDFromBytes(artistName.getBytes()));
+        artist.setName(artistName);
 
+        Music music = buildMusicEntity(musicName, artist);
         set.add(music);
-        return set;
 
+        return set;
+    }
+
+    private Music buildMusicEntity(String name, Artist a){
+        Music m = new Music();
+        m.setName(name);
+        m.setId(UUID.nameUUIDFromBytes(name.getBytes()));
+        m.setArtist(a);
+        return m;
     }
 
     public Set<MusicDTO> buildMusicSetDto() {
-
         Set<MusicDTO> set = new HashSet();
         ArtistDTO artist = new ArtistDTO();
         artist.setId(new UUID(3, 10));
@@ -88,26 +101,39 @@ class MusicServiceImplTest {
     @Test
     void findAllWithFilter() {
         String filter = "public";
+        musicService.setMusicDTOMapper(INSTANCE);
 
         Sort sort = Sort.by("artist.name").ascending()
                 .and(Sort.by("name").ascending());
 
-        Set<Music> music = buildMusicSet();
+        Set<Music> music = new HashSet<>();
+        music.addAll(buildEntitySet("Paralamas", "do Sucesso"));
+        music.addAll(buildEntitySet("Anitta", "com Sucesso"));
 
-        when(musicRepository.findAllWithFilter(filter, sort)).thenReturn(music);
+        when(musicRepository.findAllWithFilter(anyString(), any())).thenReturn(music);
 
-        musicService.setMusicDTOMapper(musicDTOMapper);
 
         Set<MusicDTO> setAtual = musicService.findAllWithFilter(filter);
 
-        Set<MusicDTO> setEsperado = musicDTOMapper.toSetOfDTO(music);
+        Set<MusicDTO> setEsperado = INSTANCE.toSetOfDTO(music);
 
+        assertEquals(setEsperado.size(), setAtual.size());
         assertEquals(setEsperado, setAtual);
     }
 
     @Test
     void whenNotFindAllWithFilterByName() {
+        musicService.setMusicDTOMapper(INSTANCE);
 
+        when(musicRepository.findAllWithFilter(anyString(), any())).thenReturn(Collections.emptySet());
+
+        MusicNotFound thrown = assertThrows(
+                MusicNotFound.class,
+                () -> musicService.findAllWithFilter("Sucesso"),
+                "Exception not found"
+        );
+
+        assertEquals(thrown.getClass(), MusicNotFound.class);
     }
 
 
