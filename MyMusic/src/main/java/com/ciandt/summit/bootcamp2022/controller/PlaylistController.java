3f290681739abebfb2dc80202b761657c76acd42
Rebecca Moraes <rgabrielle@ciandt.com>
2022-09-02 11:35:56 -0300

    BOOT-1089 Adicionar música em playlist - Alteradas rotas, passando para português, conforme documentação - Removida rota não utilizada

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/PlaylistController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/PlaylistController.java
@@ -2,11 +2,9 @@ package com.ciandt.summit.bootcamp2022.controller;
 
 import com.ciandt.summit.bootcamp2022.dto.MusicDTO;
 import com.ciandt.summit.bootcamp2022.entity.Playlist;
-import com.ciandt.summit.bootcamp2022.repository.PlaylistRepository;
 import com.ciandt.summit.bootcamp2022.service.PlaylistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,32 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 @RestController
-@RequestMapping("/api/v1/playlist")
+@RequestMapping("/api/v1/playlists")
 @RequiredArgsConstructor
 public class PlaylistController {
-
-    private final PlaylistRepository playlistRepository;
     private final PlaylistService playlistService;
 
-
-    @GetMapping("/{id}")
-    ResponseEntity<Playlist> findById(@PathVariable("id") @NotNull String id){
-        Playlist playlist = playlistRepository
-                .findAll().stream().findAny()
-                .orElseThrow(IllegalAccessError::new);
-
-        return ResponseEntity.ok(playlist);
-    }
-
-    @PostMapping("/{playlistId}/musics")
+    @PostMapping("/{playlistId}/musicas")
     ResponseEntity<Playlist> addMusicToPlaylist(
             @PathVariable("playlistId") String playlistId,
-            @Valid @RequestBody MusicDTO musics){
+            @Valid @RequestBody MusicDTO music){
         return ResponseEntity.ok(
-                playlistService.addMusicToPlaylist(playlistId, musics));
+                playlistService.addMusicToPlaylist(playlistId, music));
     }
 
 
