71216cce139c852176856308b98ef1df18fde986
Rebecca Moraes <rgabrielle@ciandt.com>
2022-09-02 11:37:17 -0300

    BOOT-1089 Adicionar música em playlist - Alterado status code da PlaylistNotFoundException para bad request - Removido import não utilizado

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exception/PlaylistNotFoundException.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exception/PlaylistNotFoundException.java
@@ -1,10 +1,9 @@
 package com.ciandt.summit.bootcamp2022.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid playlist")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid playlist")
 public class PlaylistNotFoundException extends RuntimeException{
     private static final String MSG = "Playlist was not found";
 
